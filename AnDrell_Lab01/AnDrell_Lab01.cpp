74e9668d021aa171afc20791c2fae4a26117e4ee
AnDRELL Dudley <addllerd@gmail.com>
2026-08-19 10:28:03 -0500

    Add project files.

---
+++ AnDrell_Lab01/AnDrell_Lab01.cpp
@@ -0,0 +1,13 @@
+// AnDrell_Lab01.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+// Edited directly on GitHub
+// Reviewed and updated by dyaln
+#include <iostream>
+using namespace std;
+
+int main()
+cout << "Changes made by Dylan!" << endl;
+cout << "Hello from AnDrell! This is my first Git Commit." << endl;
+cout << " Git makes collaboration easy!" << endl;
+cout << "Lab 1 completed on 08/19/26!" << endl;
+return 0;
